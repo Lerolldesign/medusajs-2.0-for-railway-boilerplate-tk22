3200efd416f74cff6238f8f3a37fc1e16d45cdb7
Lerolldesign <iam@lerolldesign.com>
2024-11-12 10:15:39 +0100

    accordion type

--- storefront/src/modules/products/components/product-tabs/index.tsx
+++ storefront/src/modules/products/components/product-tabs/index.tsx
@@ -4,8 +4,8 @@ import Back from "@modules/common/icons/back"
 import FastDelivery from "@modules/common/icons/fast-delivery"
 import Refresh from "@modules/common/icons/refresh"
 
-import Accordion from "./accordion"
 import { HttpTypes } from "@medusajs/types"
+import Accordion from "./accordion"
 
 type ProductTabsProps = {
   product: HttpTypes.StoreProduct
@@ -25,14 +25,9 @@ const ProductTabs = ({ product }: ProductTabsProps) => {
 
   return (
     <div className="w-full">
-      <Accordion type="multiple">
+      <Accordion>
         {tabs.map((tab, i) => (
-          <Accordion.Item
-            key={i}
-            title={tab.label}
-            headingSize="medium"
-            value={tab.label}
-          >
+          <Accordion.Item key={i} title={tab.label} headingSize="medium">
             {tab.component}
           </Accordion.Item>
         ))}
