d6442f4a2a35ceace2d8774213757ca5476f5b49
Lerolldesign <iam@lerolldesign.com>
2024-11-12 15:38:31 +0100

    cart button

--- storefront/src/modules/layout/templates/logoBrown/index.tsx
+++ storefront/src/modules/layout/templates/logoBrown/index.tsx
@@ -1,23 +1,40 @@
-
-import LocalizedClientLink from "@modules/common/components/localized-client-link";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Image from "next/image"
+import Link from "next/link"
+export default function Logo_brown() {
+  return (
+    <div>
+      {/**import LocalizedClientLink from "@modules/common/components/localized-client-link"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
 
 const Logo_brown = () => {
-  const router = useRouter();
+  const router = useRouter()
 
-  return ( 
-    <LocalizedClientLink
-    href="/">
-    <Image
-      className="w-[18vw] md:w-[11vw] lg:w-[6.5vw] 2xl:w-[5.5vw] left-[10%] md:left-[8%] top-[7%] cursor-pointer fixed lg:left-[5%] 2xl:left-[4%] md:top-[7%] z-50 " 
-      src="https://lunecloud.fra1.digitaloceanspaces.com/logo-ocre.png" 
-      height="100" 
-      width="100" 
-      alt="Logo" 
-    />
+  return (
+    <LocalizedClientLink href="/">
+      <Image
+        className="w-[18vw] md:w-[11vw] lg:w-[6.5vw] 2xl:w-[5.5vw] left-[10%] md:left-[8%] top-[7%] cursor-pointer fixed lg:left-[5%] 2xl:left-[4%] md:top-[7%] z-50 "
+        src="https://lunecloud.fra1.digitaloceanspaces.com/logo-ocre.png"
+        height="100"
+        width="100"
+        alt="Logo"
+      />
     </LocalizedClientLink>
-   );
+  )
 }
- 
-export default Logo_brown;
+
+export default Logo_brown
+ */}
+
+      <Link href="./">
+        <Image
+          className="w-[18vw] md:w-[11vw] lg:w-[6.5vw] 2xl:w-[5.5vw] left-[10%] md:left-[8%] top-[7%] cursor-pointer fixed lg:left-[5%] 2xl:left-[4%] md:top-[7%] z-50 "
+          src="https://lunecloud.fra1.digitaloceanspaces.com/logo-ocre.png"
+          height="100"
+          width="100"
+          alt="Logo"
+        />
+      </Link>
+    </div>
+  )
+}
--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -1,62 +1,60 @@
 import { Suspense } from "react"
 
 import { listRegions } from "@lib/data/regions"
-//import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
-//import SideMenu from "@modules/layout/components/side-menu"
+import SideMenu from "@modules/layout/components/side-menu"
 export default async function Nav() {
-  const regions = await listRegions().then((regions) => regions)
+  const regions = await listRegions()
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-0 mx-auto border-b duration-200 bg-white/0 border-ui-border-base/0 !text-lune">
+      <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
         <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
-          {/*   
-     
-       <header className="relative h-0 mx-auto border-b duration-200 bg-white/0 border-ui-border-base/0">
-      <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
-     
-     <div className="flex-1 basis-0 h-full flex items-center">
-          <div className="h-full">
-            <SideMenu regions={regions} />
+          <div className="flex-1 basis-0 h-full flex items-center">
+            <div className="h-full">
+              <SideMenu regions={regions} />
+            </div>
           </div>
-        </div>*/}
-          {/** 
-        <div className="flex items-center h-full">
-          <LocalizedClientLink
-            href="/"
-            className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-          >
-            Medusa Store
-          </LocalizedClientLink>
-        </div>*/}
 
-          <div className="flex items-center gap-x-6 h-10 flex-1 basis-0 justify-end absolute right-10 md:right-16 top-[8vh]  md:top-[8vh] !text-lune bg-browny/60 px-3 py-3 rounded-[6vw]">
-            {/**    <div className="hidden small:flex items-center gap-x-6 h-full">
-            {process.env.FEATURE_SEARCH_ENABLED && (
+          <div className="flex items-center h-full">
+            <LocalizedClientLink
+              href="/"
+              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
+              data-testid="nav-store-link"
+            >
+              Medusa Store
+            </LocalizedClientLink>
+          </div>
+
+          <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
+            <div className="hidden small:flex items-center gap-x-6 h-full">
+              {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
+                <LocalizedClientLink
+                  className="hover:text-ui-fg-base"
+                  href="/search"
+                  scroll={false}
+                  data-testid="nav-search-link"
+                >
+                  Search
+                </LocalizedClientLink>
+              )}
               <LocalizedClientLink
                 className="hover:text-ui-fg-base"
-                href="/search"
-                scroll={false}
+                href="/account"
+                data-testid="nav-account-link"
               >
-                Search
+                Account
               </LocalizedClientLink>
-            )}
-            <LocalizedClientLink
-              className="hover:text-ui-fg-base"
-              href="/account"
-            >
-              Account
-            </LocalizedClientLink>
-          </div>*/}
+            </div>
             <Suspense
               fallback={
                 <LocalizedClientLink
-                  className="hover:text-ui-fg-base flex gap-2 !z-50 !top-[3vh] !right-[5vh] !text-lune fixed"
+                  className="hover:text-ui-fg-base flex gap-2"
                   href="/cart"
+                  data-testid="nav-cart-link"
                 >
-                  <span className="  !text-lune">(0)</span>
+                  Cart (0)
                 </LocalizedClientLink>
               }
             >
