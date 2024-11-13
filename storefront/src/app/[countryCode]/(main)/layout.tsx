c80a4b72bc08aca3b3b64a6d977f50f0809aab55
Lerolldesign <iam@lerolldesign.com>
2024-11-13 16:42:39 +0100

    page presatation

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -13,7 +13,6 @@ export const metadata: Metadata = {
 export default async function PageLayout(props: { children: React.ReactNode }) {
   return (
     <>
-      {" "}
       <Ornements />
       <HeaderNew />
       {props.children}
