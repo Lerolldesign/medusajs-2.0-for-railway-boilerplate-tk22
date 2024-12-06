31bcc414a2d4463653b13cb7b309eb615ec886df
Lerolldesign <iam@lerolldesign.com>
2024-12-06 11:36:50 +0100

    pasge dashboard

--- storefront/src/app/[countryCode]/(main)/account/@dashboard/orders/page.tsx
+++ storefront/src/app/[countryCode]/(main)/account/@dashboard/orders/page.tsx
@@ -1,10 +1,10 @@
 import { Metadata } from "next"
 
-import OrderOverview from "@modules/account/components/order-overview"
-import { notFound } from "next/navigation"
 import { listOrders } from "@lib/data/orders"
+import OrderOverview from "@modules/account/components/order-overview"
 import Divider from "@modules/common/components/divider"
-import TransferRequestForm from "@modules/account/components/transfer-request-form"
+import { notFound } from "next/navigation"
+//import TransferRequestForm from "@modules/account/components/transfer-request-form"
 
 export const metadata: Metadata = {
   title: "Orders",
@@ -30,7 +30,7 @@ export default async function Orders() {
       <div>
         <OrderOverview orders={orders} />
         <Divider className="my-16" />
-        <TransferRequestForm />
+        {/**   <TransferRequestForm /> */}
       </div>
     </div>
   )
