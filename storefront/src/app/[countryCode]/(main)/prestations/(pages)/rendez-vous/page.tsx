02440fa5d2a0507d66e181b530699db57ddcee46
Lerolldesign <iam@lerolldesign.com>
2024-12-07 16:59:24 +0100

    realisation

--- storefront/src/app/[countryCode]/(main)/prestations/(pages)/rendez-vous/page.tsx
+++ storefront/src/app/[countryCode]/(main)/prestations/(pages)/rendez-vous/page.tsx
@@ -2,7 +2,7 @@ import StepRestaurer from "@modules/pages/restaurer/stepRestaurer"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Comment faire une restauration ?",
+  title: "Comment se déroule un chantier ?",
   description:
     "Réfection complète ou partielle suivant une technique semi-traditionnelle. Fauteuil, tabouret, chaise, banquette, pouf, abat-jour.",
 }
--- storefront/src/modules/pages/restaurer/stepRestaurer.tsx
+++ storefront/src/modules/pages/restaurer/stepRestaurer.tsx
@@ -7,63 +7,58 @@ const StepRestaurer: React.FC = () => {
     <div className="bg-creamy noise px-3 md:px-24 lg:px-40 pb-32 relative top-36 md:top-52">
       <div className="flex flex-col items-center">
         <h3 className="text-lune  px-[10vw] text-center font-lune text-[2.1rem] leading-9 mb:text-[3.5rem] xl:text-6xl 2xl:text-8xl relative flex flex-col">
-          Vous aimeriez restaurer un objet
+          Comment se déroule un chantier ?
         </h3>
         <p
           className="text-gray-600 md:px-5 lg:px-28 2xl:left-10 mx-10 md:mx-10 xl:mx-24 2xl:px-32 py-10 md:py-20  relative
          leading-6 2xl:leading-7 
          text-[.85rem] xl:leading-6  xl:text-[.85rem] 2xl:text-[.98rem] text-center "
         >
-          L&apos;atelier vous propose la réfection de sièges selon une technique
-          semi-traditionnelle, où la mousse remplace le crin. Le garnissage
-          contemporain suit de nombreuses étapes identiques aux méthodes
-          traditionnelles, tout en appliquant des techniques spécifiques à la
-          mousse d&apos;une grande technicité. Nous réalisons également toutes
-          sortes de restaurations, que ce soit pour abats-jour, sièges, ou
-          textiles. Chaque pièce est traitée avec soin et expertise pour lui
-          redonner son éclat d&apos;antan tout en respectant les méthodes
-          modernes.
+          Que ce soit pour la création d’un abat-jour,la confections de vos
+          décors textiles, ou la restauration d’un siège, voici comment se
+          déroulera votre chantier
         </p>
       </div>
 
       <Accordion title="Prise de contact ">
         <p className="text-[.70rem] xl:leading-6  xl:text-[.85rem] 2xl:text-[.98rem]  px-[20vw] pb-10">
-          Téléphone (visio) ou mail déterminer la nature de votre projet je vous
-          accueille pour un 1erRDV soit à l&apos;atelier soit à votre domicile
-          pour discuter de votre projet, il est préférable de venir avec votre
-          siège, une photo peut également nous permettre de faire une 1ère
-          évaluation
+          Nous échangerons par téléphone ou mail pour déterminer la nature de
+          votre projet. L’envoi d’une photo peut me permettre de faire une 1
+          <sup>ère</sup>
+          évaluation. A ce moment nous prendrons rendez-vous.
         </p>
       </Accordion>
 
-      <Accordion title="Proposition d'un devis et délai">
+      <Accordion title="Premier rendez-vous">
         <p className="text-[.70rem] xl:leading-6  xl:text-[.85rem] 2xl:text-[.98rem]  px-[20vw] pb-10">
-          Mon tarif varie en fonction du type de projets et des choix de tissus.
-          Pour un fauteuil Voltaire, comptez environ 350€ + achat du tissu et
-          des finitions pour le changement de la couverture et environ 750€ +
-          achat du tissu et des finitions pour une réfection complète. Si vous
-          fournissez le tissu, le coût de la façon sera majoré et le tissu ne
-          sera bien sûr pas garanti.
+          Nous nous rencontrerons pour ce 1<sup>er</sup> rendez-vous soit à
+          l'atelier soit à votre domicile. Si il s’agit d’une réfection de
+          siège, ou une restauration d’abat-jour, il est préférable de venir
+          avec l'objet. Mon tarif varie en fonction du type de projet et du
+          choix de tissus. Si vous fournissez le tissu, le coût de la façon sera
+          majoré et le tissu ne sera pas garanti. Je me laisse également la
+          liberté de travailler ou non avec votre tissu si je ne le juge pas
+          adapté à votre projet
         </p>
       </Accordion>
       <Accordion title="Dépôt de l’objet à l’atelier">
         <p className="text-[.70rem] xl:leading-6  xl:text-[.85rem] 2xl:text-[.98rem]  px-[20vw] pb-10">
-          Vous pouvez venir déposer vos objets une fois restauré directement à
-          l&apos;atelier. Un enlévement est également envisageable des frais de
-          déplacement seront facturés
+          Une fois le devis accepté vous pouvez venir déposer votre objet
+          directement à l'atelier. Un enlèvement à domicile est envisageable
+          mais des frais de déplacement pourront être facturés.
         </p>
       </Accordion>
       <Accordion title="Réalisation">
         <p className="text-[.70rem] xl:leading-6  xl:text-[.85rem] 2xl:text-[.98rem]  px-[20vw] pb-10">
-          Vous disposerez d&apos;un suivi dans les étapes de réalisation de
-          votre projet.
+          Si vous le souhaitez, vous disposerez d'un suivi dans les étapes de
+          réalisation de votre projet.
         </p>
       </Accordion>
       <Accordion title="Retrait ou livraison">
         <p className="text-[.70rem] xl:leading-6  xl:text-[.85rem] 2xl:text-[.98rem]  px-[20vw] pb-10">
           Vous pouvez venir retirer votre objet une fois restauré directement à
-          l&apos;atelier. Une livraison est également envisageable toutefois au
-          delà de 40km de mon atelier, des frais de déplacement seront facturés.
+          l'atelier. Une livraison est également envisageable toutefois des
+          frais de déplacement seront à envisager.
         </p>
       </Accordion>
       <div>
@@ -73,7 +68,7 @@ const StepRestaurer: React.FC = () => {
                             duration-400 hover:translate-y-[-6px] hover:text-creamy hover:rounded-full hover:bg-lune hover:shadow-[4px_4px_4px_lune] active:translate-x-[0px] 
                             active:translate-y-[0px] active:rounded-2xl active:shadow-none"
           >
-            Prendre rendez-vous
+            + de questions ?
           </button>
         </Link>
       </div>
